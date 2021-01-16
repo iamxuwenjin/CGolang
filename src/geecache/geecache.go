333f303d1f9dafc0fb6b2250d46b44558f548d79
wenjin.xu <wenjin.xu@moji.com>
2021-01-16 11:49:39 +0800

    add gee cache

--- src/geecache/geecache.go
+++ src/geecache/geecache.go
@@ -1,6 +1,10 @@
 package geecache
 
-import "sync"
+import (
+	"fmt"
+	"log"
+	"sync"
+)
 
 // A Getter loads data for a key.
 type Getter interface {
@@ -48,4 +52,39 @@ func GetGroup(name string) *Group {
 	g := groups[name]
 	mu.RUnlock()
 	return g
+}
+
+// 从Group中查找key
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	// 命中缓存直接返回
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+
+	// 未命中则调用回调函数，获取源数据
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
 }
--- src/geecache/geecache_test.go
+++ src/geecache/geecache_test.go
@@ -1,6 +1,8 @@
 package geecache
 
 import (
+	"fmt"
+	"log"
 	"reflect"
 	"testing"
 )
@@ -12,5 +14,41 @@ func TestGetterFunc_Get(t *testing.T) {
 	expect := []byte("key")
 	if v, _ := f.Get("key"); !reflect.DeepEqual(v, expect) {
 		t.Errorf("callback failed")
+	}
+}
+
+var db = map[string]string{
+	"Tom":  "666",
+	"Jack": "666",
+	"Sam":  "666",
+}
+
+func TestGroup_Get(t *testing.T) {
+	// 用来记录key被调用了几次
+	loadCounts := make(map[string]int, len(db))
+	gee := NewGroup("scores", 2<<10, GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[slowDB] search key", key)
+			if v, ok := db[key]; ok {
+				if _, ok := loadCounts[key]; !ok {
+					loadCounts[key] = 0
+				}
+				loadCounts[key] += 1
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+	for k, v := range db {
+		view, err := gee.Get(k)
+		if err != nil || view.String() != v {
+			t.Fatalf("failed to get value of %s", k)
+		}
+		_, err = gee.Get(k)
+		if err != nil || loadCounts[k] > 1 {
+			t.Fatalf("cache %s miss", k)
+		}
+	}
+	if view, err := gee.Get("unknown"); err == nil {
+		t.Fatalf("the value of unknow should be empty, but %s got", view)
 	}
 }
